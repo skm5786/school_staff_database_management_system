f72552058ed73b24a83372db6b0b57256e2c9eb5
skm5786 <sumitthakral3@gmail.com>
2022-07-25 11:07:37 +0530

    Initial Commit

---
+++ school staff database management system/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  school staff database management system
+//
+//  Created by sumit thakral on 25/07/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
