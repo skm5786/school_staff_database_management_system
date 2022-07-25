11a1be3b6a961b229c9d686d35f7bc06cd1d7fa2
skm5786 <sumitthakral3@gmail.com>
2022-07-25 11:47:19 +0530

    main

--- school staff database management system/main.cpp
+++ school staff database management system/main.cpp
@@ -1,14 +1,219 @@
-//
-//  main.cpp
-//  school staff database management system
-//
-//  Created by sumit thakral on 25/07/22.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <string>
+#include <iomanip>
+using namespace::std;
+class staff{
+protected:
+    int code;
+    string name;
+protected:
+    staff(){
+        code=0;
+        name="0";
+    }
+    staff(int a,string p){
+        code =a;
+        name=p;
+    }
+    void showcode(){
+        cout<<"code is "<<code<<"\n";
+    }
+    void showname(){
+        cout<<"name is "<<name<<"\n";
+    }
+};
+class educational_qualification{
+protected:
+    string generaleducation;
+protected:
+    educational_qualification(){
+        generaleducation="0";
+    }
+    educational_qualification(string e){
+        generaleducation=e;
+    }
+    void showgeneraleducation(){
+        cout<<"general education is "<<generaleducation<<"\n";
+    }
+};
+    class teacher:protected staff,protected educational_qualification{
+        string subject;
+        string publication;
+    public:
+        void getinfo();
+        teacher(){};
+        void showinfo(){
+           
+            showcode();
+            showname();
+            cout<<"subject of teacher is "<<subject<<"\n";
+            cout<<"publication of teacher is "<<publication<<"\n";
+            showgeneraleducation();
+        }
+        
+    };
+class officer:protected staff,protected educational_qualification{
+    int grade;
+public:
+    officer(){};
+    void getinfo();
+    void showinfo(){
+        showcode();
+        showname();
+        cout<<"grade of officer is "<<grade<<"\n";
+        showgeneraleducation();
+    }
+};
+class typist: protected staff{
+protected:
+    int speed;
+protected:
+    typist(){};
+    void showspeed(){
+        cout<<"speed of typist in words per minute is "<<speed<<"\n";
+    }
+};
+class regulartypist:protected typist{
+public:
+    regulartypist(){};
+    void getinfo();
+    void showinfo(){
+        showcode();
+        showname();
+        showspeed();
+    }
+    
+};
+class casualtypist:protected typist{
+    int dailywages;
+public:
+    casualtypist(){};
+    void getinfo();
+    void showinfo(){
+        showcode();
+        showname();
+        showspeed();
+        cout<<"daily wages of casual typist is "<<dailywages<<"\n";
+    }
+    
+};
+void teacher::getinfo(){
+    
+    cout<<"enter teacher code: ";
+    cin>>code;
+    cout<<"name of teacher: ";
+    cin>>name;
+    cout<<"enter subject of teacher: ";
+    cin>>subject;
+    cout<<"enter publication of teacher: ";
+    cin>>publication;
+    cout<<"enter general education of teacher: ";
+    cin>>generaleducation;
+}
+void officer::getinfo(){
+    cout<<"enter officer code: ";
+    cin>>code;
+    cout<<"name of officer: ";
+    cin>>name;
+    cout<<"enter grade of officer: ";
+    cin>>grade;
+    cout<<"enter general education of officer: ";
+    cin>>generaleducation;
+}
+void regulartypist::getinfo(){
+    cout<<"enter regulartypist code: ";
+    cin>>code;
+    cout<<"name of regulartypist: ";
+    cin>>name;
+    cout<<"enter speed of regulartypist: ";
+    cin>>speed;
+    
+}
+void casualtypist::getinfo(){
+    cout<<"enter casualtypist code: ";
+    cin>>code;
+    cout<<"name of casualtypist: ";
+    cin>>name;
+    cout<<"enter speed of casualtypist: ";
+    cin>>speed;
+    cout<<"enter dailywages of casualtypist: ";
+    cin>>dailywages;
+}
+int main(){
+    regulartypist a[50];
+    casualtypist b[50];
+    teacher c[50];
+    officer d[50];
+    int k=0,i=0,j=0;
+ /*   regulartypist a;
+    a.getinfo();
+    cout<<"\n";
+    a.showinfo();
+    cout<<"\n";
+    casualtypist b;
+    b.getinfo();
+    cout<<"\n";
+    b.showinfo();
+    cout<<"\n";
+    teacher c;
+    c.getinfo();
+    cout<<"\n";
+    c.showinfo();
+    cout<<"\n";
+    c.showinfo();
+    cout<<"\n";
+    officer d;
+    d.getinfo();
+    cout<<"\n";
+    d.showinfo();
+    cout<<"\n";  */
+    while (1) {
+        cout<<setw(40)<<"School database system\n";
+        cout<<setw(40)<<"______________________\n";
+        cout<<setw(54)<<"press 1 to enter regular typist data\n";
+        cout<<setw(53)<<"press 2 to enter casual typist data\n";
+        cout<<setw(47)<<"press 3 to enter teacher data\n";
+        cout<<setw(47)<<"press 4 to enter officer data\n";
+        cout<<setw(54)<<"press 5 to show regular typist data\n";
+        cout<<setw(53)<<"press 6 to show casual typist data\n";
+        cout<<setw(47)<<"press 7 to show teacher data\n";
+        cout<<setw(47)<<"press 8 to show officer data\n";
+        cout<<setw(47)<<"press 9 to exit\n";
+        cin>>k;
+        switch (k) {
+            case 1:
+                cout<<"enter regular typist "<<i+1<<"data\n";
+                a[i].showinfo();
+                i++;
+                break;
+            case 2:
+                cout<<"enter casual typist "<<i+1<<"data\n";
+                b[i].showinfo();
+                i++;
+            case 3:
+                cout<<"enter teacher "<<i+1<<"data\n";
+                c[i].showinfo();
+                i++;
+            case 4:
+                cout<<"enter officer "<<i+1<<"data\n";
+                d[i].showinfo();
+                i++;
+            case 5:
+                cout<<"enter regulartypist number\n";
+                cin>>j;
+                a[j-1].showinfo();
+            case 6:
+                cout<<"enter casualtypist number\n";
+                cin>>j;
+                a[j-1].showinfo();
+            case 7:
+                cout<<"enter teacher number\n";
+                cin>>j;
+                a[j-1].showinfo();
+            default:
+                break;
+        }
+    }
+    
     return 0;
 }
